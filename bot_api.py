4e983548b9a9bec3a410ea043ce57c25b4b1945f
Cheng Tan <noinil@gmail.com>
2016-09-14 09:46:40 +0900

    local test init.

--- bot_api.py
+++ bot_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -11,6 +11,12 @@ deepThought.set_trainer(ChatterBotCorpusTrainer)
 # 使用中文语料库训练它
 # 只需要训练一次，不需要每次启动进程都训练，训练结果默认存到本地`./database.db`,之后启动进程会使用这个数据库
 deepThought.train("chatterbot.corpus.chinese")  # 语料库
+deepThought.train("chatterbot.corpus.english")  # 语料库
+deepThought.train("chatterbot.corpus.chinese.greetings")  # 语料库
+deepThought.train("chatterbot.corpus.english.greetings")  # 语料库
+deepThought.train("chatterbot.corpus.chinese.conversations")  # 语料库
+deepThought.train("chatterbot.corpus.english.conversations")  # 语料库
+deepThought.train("chatterbot.corpus.chinese.trivia")  # 语料库
 
 
 @hug.get()
--- wechat_bot.py
+++ wechat_bot.py
@@ -6,13 +6,74 @@ import requests
 bot_api="http://127.0.0.1:8000/get_response"
 
 class MyWXBot(WXBot):
+    def __init__(self):
+        WXBot.__init__(self)
+        self.robot_switch = True
+
+    def auto_switch(self, msg):
+        msg_data = msg['content']['data']
+        stop_cmd = [u'机器人退下']
+        start_cmd = [u'三花聚顶', u'飞龙在天', u'反清复明']
+        if self.robot_switch:
+            for i in stop_cmd:
+                if i == msg_data:
+                    self.robot_switch = False
+                    self.send_msg_by_uid(u'[BOT] ' + u'机器人已关闭！', msg['to_user_id'])
+        else:
+            for i in start_cmd:
+                if i == msg_data:
+                    self.robot_switch = True
+                    self.send_msg_by_uid(u'[BOT] ' + u'机器人已开启！', msg['to_user_id'])
+
     def handle_msg_all(self, msg):
-        if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
+        if not self.robot_switch and msg['msg_type_id'] != 1:
+            return
+        if msg['msg_type_id'] == 1 and msg['content']['type'] == 0:  # reply to self
+            self.auto_switch(msg)
+        elif msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
             user_input = msg["content"]["data"]
             payload={"user_input":user_input}
+            reply = '[BOT] '
             response = requests.get(bot_api,params=payload).json()["response"]
-            #print(type(response)) # unicode
-            self.send_msg_by_uid(response, msg['user']['id'])
+            reply += response
+            self.send_msg_by_uid(reply, msg['user']['id'])
+        elif msg['msg_type_id'] == 3 and msg['content']['type'] == 0:  # group text message
+            if 'detail' in msg['content']:
+                my_names = self.get_group_member_name(self.my_account['UserName'], msg['user']['id'])
+                if my_names is None:
+                    my_names = {}
+                if 'NickName' in self.my_account and self.my_account['NickName']:
+                    my_names['nickname2'] = self.my_account['NickName']
+                if 'RemarkName' in self.my_account and self.my_account['RemarkName']:
+                    my_names['remark_name2'] = self.my_account['RemarkName']
+
+                is_at_me = False
+                for detail in msg['content']['detail']:
+                    if detail['type'] == 'at':
+                        for k in my_names:
+                            if my_names[k] and my_names[k] == detail['value']:
+                                is_at_me = True
+                                break
+
+                if is_at_me:
+                    print(msg)
+                    src_name = msg['content']['user']['name']
+                    reply = '[BOT] @' + src_name + ' : '
+                    if msg['content']['type'] == 0:  # text message
+                        user_input = msg["content"]["desc"]
+                        payload={"user_input":user_input}
+                        response = requests.get(bot_api,params=payload).json()["response"]
+                        reply += response
+                    else:
+                        reply += u"对不起，只认字，其他杂七杂八的我都不认识，,,Ծ‸Ծ,,"
+                    self.send_msg_by_uid(reply, msg['user']['id'])
+        elif msg['msg_type_id'] == 5 and msg['content']['type'] == 0:
+            print(msg)
+            if msg['user']['name'] == u'小冰':
+                user_input = msg["content"]["data"]
+                payload={"user_input":user_input}
+                response = requests.get(bot_api,params=payload).json()["response"]
+                self.send_msg_by_uid(response, msg['user']['id'])
 
 def main():
     bot = MyWXBot()
